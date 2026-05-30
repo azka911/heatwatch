27d12031940d4548dd4364736b9964097ddab9b4
azka911 <22118852@siswa.um.edu.my>
2026-05-30 11:50:32 +0800

    Add Vercel domain to CORS

--- heatwatch-api/main.py
+++ heatwatch-api/main.py
@@ -13,7 +13,10 @@ app = FastAPI(title="HeatWatch API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://heatwatch-chi.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
